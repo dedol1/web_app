ff4a1175f42c6507cc5e0a2749f585dad8025a8b
root <root@localhost.localdomain>
2019-07-18 14:42:20 +0000

    update

--- myapp.py
+++ myapp.py
@@ -2,11 +2,36 @@ from flask import Flask, render_template
 #
 app = Flask(__name__)
 @app.route('/')
-
 def index():
         return render_template('index.html')
         
         
+        
+
+@app.route('/about.html')
+
+def about():
+        return render_template('about.html')
+
+
+
+@app.route('/products.html')
+
+def product():
+        return render_template('products.html')
+
+
+@app.route('/store.html')
+
+def store():
+        return render_template('store.html')
+        
+@app.route('/index.html')
+
+def home():
+        return render_template('index.html')
+        
+
         
         
         
